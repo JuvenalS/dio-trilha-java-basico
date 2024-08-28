b39e14fc7a3d30738dc2b8009c90597a1d2533f6
Juvenal Silveira da Silva Neto <silveiraneto@hotmail.com>
2024-08-28 09:46:39 -0300

    captura de dados do usuário e apresentação

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String cliente;
+        Double saldo;
+
+        System.out.print("Digite o número da conta: ");
+        numero = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Digite o número da agência: ");
+        agencia = sc.nextLine();
+
+        System.out.print("Digite o nome do cliente: ");
+        cliente = sc.nextLine();
+
+        System.out.print("Digite o saldo da conta: ");
+        saldo = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.print("Olá " + cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.\n");
+
+        sc.close();
     }
 }
